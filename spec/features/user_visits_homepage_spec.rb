e9d0a686531df503707c78083d851c42824b4aaa
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-05 09:34:31 -0600

    Update gemfile

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'user visits homepage' do
   scenario 'unsigned user visits homepage' do
     visit root_path
-    expect(page).to have_content 'welcome to rails'
+    expect(page).to have_content 'home'
   end
 end
