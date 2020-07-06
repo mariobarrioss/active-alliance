577b24214c7b8146b7a0cf5cbb3f8085789c71f1
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-06 14:55:25 -0600

    Update navbar and activities_controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,31 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: %i[show edit update destroy]
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = current_user.activities.build(activity_params)
+
+    respond_to do |format|
+      if @activity.save
+        format.html { redirect_to current_user, notice: 'A new activity was added to all your activities.' }
+        format.json { render :show, status: :created, location: @activity }
+      else
+        format.html { render :new }
+        format.json { render json: @activity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:description, :amount)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home', to: 'static_pages#home'
   get 'static_pages/about', to: 'static_pages#about', as: :about
   root 'static_pages#home'
+
+  resources :activities
+  resources :groups
 end
