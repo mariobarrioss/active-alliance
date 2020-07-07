bb366aa841883d47b28769be112ed93743152745
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-07 14:08:59 -0600

    Add styling for groups show view

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,13 @@
 class Group < ApplicationRecord
   has_many :activities, class_name: 'Activity', foreign_key: :activity_id
   has_one_attached :icon
+
+  validates :icon, content_type: { in: %w[image/jpeg image/gif image/png],
+                                   message: 'must be a valid image format' },
+                   size: { less_than: 5.megabytes,
+                           message: 'should be less than 5MB' }
+
+  def display_icon
+    icon.variant(resize: '500x500')
+  end
 end
