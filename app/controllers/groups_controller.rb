78c32c5260261ea047a0c029112f68d3ae365b6b
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-07 10:50:44 -0600

    Add image upload functionality for group icons

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,13 +9,19 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def create
-    @group = Group.build(group_params)
+    @group = Group.new(group_params)
+    @group.icon.attach(params[:group][:icon])
+    @group.save
 
     respond_to do |format|
       if @group.save
         format.html { redirect_to root_path, notice: 'The group was created successfully.' }
-        # format.json { render :show, status: :created, location: @group }
+        format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new }
         format.json { render json: @activity.errors, status: :unprocessable_entity }
@@ -26,6 +32,6 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name, :icon)
+    params.require(:group).permit(:name, :description, :icon)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
   has_many :activities, class_name: 'Activity', foreign_key: :activity_id
+  has_one_attached :icon
 end
