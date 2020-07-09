03c3e5400579ae5696b170c96cbce85a2487f688
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-09 12:55:21 -0600

    Add helper method to calculate total number of minutes in activities

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,13 @@
 module UsersHelper
   def activities?(activities)
-    return if activities.empty?
+    return if activities.nil?
 
     render activities
+  end
+
+  def total(activities)
+    return 0 if activities.nil?
+
+    activities.sum(&:amount)
   end
 end
