963c63e69b1beb439b75d1ad59f3bfc4642bbff8
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-10 10:24:56 -0600

    Add  Like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_144437) do
+ActiveRecord::Schema.define(version: 2020_07_10_162117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2020_07_08_144437) do
     t.text "description"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "activity_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_likes_on_activity_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -71,4 +80,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_144437) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "groups"
   add_foreign_key "activities", "users", column: "author_id"
+  add_foreign_key "likes", "activities"
+  add_foreign_key "likes", "users"
 end
---
+++ spec/factories/likes.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :like do
+    activity { nil }
+    user { nil }
+  end
+end
