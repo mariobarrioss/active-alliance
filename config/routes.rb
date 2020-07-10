a986425cbdd4a3e86c4a0f13feb457ed43563426
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-10 15:24:30 -0600

    Remove static_pages and excess files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/home', to: 'static_pages#home'
-  get 'static_pages/about', to: 'static_pages#about', as: :about
+  
   get 'profile', to: 'users#show'
   get 'personal', to: 'users#personal'
   get 'external', to: 'users#external'
--- spec/factories/likes.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :like do
-    activity { nil }
-    user { nil }
-  end
-end
