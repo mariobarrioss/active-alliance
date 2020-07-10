d349f421210876446f2e7c05a2f00be1af9680ab
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-10 14:06:20 -0600

    Add validations to models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
+  validates :description, presence: true, length: { maximum: 50,
+                                                    too_long: 'Describe your activity in less than 50 characters.' }
+
   validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 480 }
 
   belongs_to :author, class_name: 'User', foreign_key: :author_id
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,13 +1,16 @@
 class Group < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 75 }
+  validates :icon, presence: true,
+                   content_type: { in: %w[image/jpeg image/gif image/png],
+                                   message: 'must be a valid image format' },
+                   size: { less_than: 5.megabytes,
+                           message: 'should be less than 5MB' }
+
   has_many :activities
   has_one_attached :icon
 
   scope :alphabetical, -> { order(:name) }
-
-  validates :icon, content_type: { in: %w[image/jpeg image/gif image/png],
-                                   message: 'must be a valid image format' },
-                   size: { less_than: 5.megabytes,
-                           message: 'should be less than 5MB' }
 
   def display_icon
     icon.variant(resize: '400x400')
