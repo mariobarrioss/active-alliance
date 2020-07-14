ec3d7cd61cdce4b860d24414f0986719603bb9e0
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-14 11:03:03 -0600

    Refactor likes controller to handle success and failure in create and destroy actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,24 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.create(activity_id: params[:activity_id])
     like.save
+
+    if like.save
+      flash[:notice] = 'Great! keep encouraging healthy activities'
+    else
+      flash[:alert] = 'You cannot like this post'
+    end
     redirect_to request.referrer
   end
 
   def destroy
     like = Like.find_by(id: params[:id], user: current_user, activity_id: params[:activity_id])
     like.destroy
+
+    if like.destroy
+      flash[:alert] = 'You disliked an activity!'
+    else
+      flash[:alert] = 'You cannot dislike a post that was not previously liked!'
+    end
     redirect_to request.referrer
   end
 end
