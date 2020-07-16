9289f307b7c6e78cfc20521975d76f0e6fde14e7
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-16 09:46:21 -0600

    Refactorr like helper method in application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,22 +17,16 @@ module ApplicationHelper
     end
   end
 
-  def like_or_dislike(activity)
+  def like(activity)
     like = Like.find_by(activity: activity, user: current_user)
     if like
-      link_to('Dislike!', activity_like_path(id: like.id, activity_id: activity.id), class: 'has-text-grey',
-                                                                                     method: :delete)
+      link_to(activity_like_path(id: like.id, activity_id: activity.id), class: 'level-item', method: :delete) do
+        content_tag(:span, content_tag(:i, '', class: 'fas fa-heart'), class: 'icon is-small has-text-danger')
+      end
     else
-      link_to('Like!', activity_likes_path(activity_id: activity.id), class: 'has-text-grey', method: :post)
-    end
-  end
-
-  def like_or_dislike_icon(activity)
-    like = Like.find_by(activity: activity, user: current_user)
-    if like
-      content_tag(:span, content_tag(:i, '', class: 'fas fa-heart'), class: 'icon is-small has-text-danger')
-    else
-      content_tag(:span, content_tag(:i, '', class: 'fas fa-heart'), class: 'icon is-small has-text-grey')
+      link_to(activity_likes_path(activity_id: activity.id), class: 'level-item', method: :post) do
+        content_tag(:span, content_tag(:i, '', class: 'fas fa-heart'), class: 'icon is-small has-text-grey')
+      end
     end
   end
 end
