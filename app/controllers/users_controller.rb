fa8d650c59340c16880839e1cacc8ed6040fb2ce
Mario Barrios <mariobarriossalazar@outlook.com>
2020-07-26 12:42:16 -0600

    Refactor users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
+  before_action :set_activities, only: %i[personal external]
 
   def show; end
 
-  def personal
-    @personal_activities = Activity.most_recent.includes(:author, group: [icon_attachment: :blob]).where(author_id:
-      @user.id)
-  end
+  def personal; end
 
-  def external
-    @external_activities = Activity.most_recent.ungrouped.includes(:author).where(author_id: @user.id)
-  end
+  def external; end
 
   private
 
   def set_user
     @user = current_user
+  end
+
+  def set_activities
+    @personal_activities = Activity.most_recent.includes(:author).where(author_id:
+      @user.id)
+    @external_activities = @personal_activities.select { |activity| activity.group_activities.count.zero? }
   end
 end
