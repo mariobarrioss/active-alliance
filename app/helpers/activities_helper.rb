0598a23b2dea948bed0632c2de7caf87afacac0f
Mario Barrios <mariobarriossalazar@outlook.com>
2020-08-02 11:11:27 -0600

    Add helper method to activities too only allow the author of an activity to do group assignments

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -12,4 +12,10 @@ module ActivitiesHelper
 
     link_to('Assigned Groups', controller: 'group_activities', action: 'index', id: activity.id)
   end
+
+  def group_assign_link?(user, activity)
+    return if user != activity.author
+
+    link_to('Assign activity to a group', controller: 'group_activities', action: 'new', id: activity.id)
+  end
 end
